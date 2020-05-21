41cdd064c722940b2847b4e36f309c56db04791a
Arthur AM <arthrfrnco@gmail.com>
2020-05-21 13:11:05 -0300

    update

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 DEST="$HOME/.local/bin"
 
-FILENAME="koan.sh"
+FILENAME="koan"
 
 cd $DIR; chmod +x "$FILENAME"; cp $FILENAME $DEST
